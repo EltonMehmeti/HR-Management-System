b02e31efb944a689a9619566922b723c394ed0a9
elton <eltonmhmt@gmail.com>
2024-03-24 00:01:14 +0100

    Setup react app

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center flex-row  h-screen w-screen bg-red-600">
+  <div>
+
+  <h1 className="text-3xl  font-bold underline bg-slate-800">
+      Hello world!
+    </h1>
+  </div>
+    <div>
+      <h1 className="bg-green-500 text-4xl">Ku jeni ekipa ma e fort nUBT, Suksese!</h1>
+    </div>
+    </div>
+  );
+}
+
+export default App;
