da9d4ebaa0fd2c89353e967ec2600ae4abaf6cee
Blendi Miftari <119363582+blendimiftari@users.noreply.github.com>
2024-03-28 16:04:13 +0100

    Created the team CRUD

--- server/app.js
+++ server/app.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const sequelize = require('./util/database');
-const mongoConnect = require('./util/mongoCon');
-const employeeRoutes = require('./routes/employee');
-const employeeAuthRoutes = require('./routes/auth/employee');
-const app = express();
-const PORT = 3001;
-const employee = require('./models/employee');
-app.use(bodyParser.json());
+const express = require("express")
+const bodyParser = require("body-parser")
+const sequelize = require("./util/database")
+const mongoConnect = require("./util/mongoCon")
+const employeeRoutes = require("./routes/employee")
+const employeeAuthRoutes = require("./routes/auth/employee")
+const teamRoutes = require("./routes/team")
+const app = express()
+const PORT = 3001
+const employee = require("./models/employee")
+app.use(bodyParser.json())
 async function startServer() {
   try {
-    await sequelize.sync({ logging: console.log });
-    console.log('Database synchronized successfully');
+    await sequelize.sync({ logging: console.log })
+    console.log("Database synchronized successfully")
     app.listen(PORT, () => {
-      console.log(`Server is running and listening on port ${PORT}`);
-    });
+      console.log(`Server is running and listening on port ${PORT}`)
+    })
   } catch (error) {
-    console.error('Error occurred while synchronizing database:', error);
+    console.error("Error occurred while synchronizing database:", error)
   }
 }
 
 mongoConnect(() => {
-  console.log('Connected to MongoDB');
-});
+  console.log("Connected to MongoDB")
+})
 
-app.use('/employee', employeeRoutes);
-app.use('/auth/employee', employeeAuthRoutes);
-startServer();
+app.use("/employee", employeeRoutes)
+app.use("/auth/employee", employeeAuthRoutes)
+app.use("/team", teamRoutes)
+startServer()
---
+++ server/controllers/team.js
@@ -0,0 +1,79 @@
+const Team = require("../models/team")
+
+//Get all Teams
+const getAllTeams = async (req, res) => {
+  try {
+    const teams = await Team.findAll()
+    res.json(teams)
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+//Get Team By Id
+const getTeamById = async (req, res) => {
+  const { id } = req.params
+  try {
+    const team = await Team.findByPk(id)
+    if (!team) {
+      return res.status(404).json({ error: "Team not found" })
+    }
+    res.json(team)
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+//Create new Team
+const createTeam = async (req, res) => {
+  console.log(req.body)
+  const { name, description, leaderId } = req.body
+  try {
+    const team = await Team.create({ name, description, leaderId })
+    res.status(201).json(team)
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+//Update a Team
+const updateTeam = async (req, res) => {
+  const { id } = req.params
+  const { name, description, leaderId } = req.body
+  try {
+    const [updated] = await Team.update(
+      { name, description, leaderId },
+      { where: { id } }
+    )
+    if (!updated) {
+      return res.status(404).json({ error: "Team not found" })
+    }
+    const updatedTeam = await Team.findByPk(id)
+    res.json(updatedTeam)
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+//Delete a Team
+const deleteTeam = async (req, res) => {
+  const { id } = req.params
+  try {
+    const deletedTeam = await Team.findByPk(id)
+    if (!deletedTeam) {
+      return res.status(404).json({ error: "Team not found" })
+    }
+    await Team.destroy({ where: { id } })
+    res.json({ message: "Team deleted successfully" })
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+module.exports = {
+  getAllTeams,
+  getTeamById,
+  createTeam,
+  updateTeam,
+  deleteTeam,
+}
