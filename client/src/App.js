d3d362a726e2d36db3929bb0d075ae7f98cc5a82
Syartaa <syartapajaziti@gmail.com>
2024-03-30 22:12:58 +0100

    adding root to the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Layouts/Dashboard/pages/Dashboard';
 import './index.css';
+import Signin from './Layouts/Dashboard/pages/Signin';
+import Signup from './Layouts/Dashboard/pages/Signup';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         {/* Add more routes here if needed */}
+        <Route path="/dashboard/signin" element={<Signin />} />
+        <Route path="/dashboard/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
