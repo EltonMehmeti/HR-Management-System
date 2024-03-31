8f1319dfd9180266c99a3fe1c4d9ec7f0d32e0e5
jonakrs <jonakrasniqi1411@gmail.com>
2024-03-31 17:02:20 +0200

    created signup page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Layouts/Dashboard/pages/Dashboard';
 import './index.css';
+import Signup from './components/auth/Signup';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
         {/* Add more routes here if needed */}
       </Routes>
     </Router>
