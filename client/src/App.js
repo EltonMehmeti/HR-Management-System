7fb0fb4f6c295dba09de41c8e0221193859dad15
jonakrs <jonakrasniqi1411@gmail.com>
2024-04-02 13:22:52 +0200

    Fixed the api calls of employee auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Layouts/Dashboard/pages/Dashboard';
 import './index.css';
-import Signup from './components/auth/Employee/Signup';
-import Login from './components/auth/Employee/Login';
-
+import EmpSignup from './components/auth/Employee/Signup';
+import EmpSignin from './components/auth/Employee/Signin';
+import HrSignin from './Layouts/Dashboard/pages/Signin';
+import HrSignup from './Layouts/Dashboard/pages/Signup';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/employee/signup" element={<Signup />} />
-        <Route path="/employee/login" element={<Login/>} />
+        <Route path="/employee/signup" element={<EmpSignup />} />
+        <Route path="/employee/signin" element={<EmpSignin/>} />
 
         {/* Add more routes here if needed */}
-        <Route path="/dashboard/signin" element={<Signin />} />
-        <Route path="/dashboard/signup" element={<Signup />} />
+        <Route path="/hr/signin" element={<HrSignin />} />
+        <Route path="/hr/signup" element={<HrSignup />} />
       </Routes>
     </Router>
   );
--- client/src/Layouts/Dashboard/pages/Signup.jsx
+++ client/src/Layouts/Dashboard/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Signup() {
+export default function HrSignup() {
   const [formData, setFormData] = useState({
     name: "",
     role:'',
