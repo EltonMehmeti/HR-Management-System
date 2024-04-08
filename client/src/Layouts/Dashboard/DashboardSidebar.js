56a0ca8c76d09cb6684a326f75230e298dc978cf
Syartaa <syartapajaziti@gmail.com>
2024-04-08 15:11:18 +0200

    modifying the sidebar

--- client/src/Layouts/Dashboard/DashboardSidebar.js
+++ client/src/Layouts/Dashboard/DashboardSidebar.js
@@ -1,46 +1,65 @@
-import React from "react";
-import { Sidebar } from "flowbite-react";
+
+import React from 'react';
 import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser } from "react-icons/hi";
-import logo from "../../Image/logo (1).png"
+import { Link } from 'react-router-dom';
 
-export default function SideBar() {
-  return (
-   
-    <Sidebar aria-label="Sidebar with multi-level dropdown example" className="bg-sky-800">
-     
-      <Sidebar.Items>
-        <Sidebar.ItemGroup>
-        <Sidebar.Item>
-        <img src={logo} alt="Logo" className="w-10 h-15 mx-auto my-4" /> {/* Adjust the class to fit your styling */}
-      </Sidebar.Item>
-          <Sidebar.Item href="#" className="text-white" icon={HiChartPie}>
-            Dashboard
-          </Sidebar.Item>
-          <Sidebar.Collapse icon={HiShoppingBag} className="text-white" label="EMployees">
-            <Sidebar.Item href="dashboard/employee" className="mr-auto font-normal text-white">Employees</Sidebar.Item>
-            <Sidebar.Item href="#" className="text-white">Sales</Sidebar.Item>
-            <Sidebar.Item href="#" className="text-white">Refunds</Sidebar.Item>
-            <Sidebar.Item href="#" className="text-white">Shipping</Sidebar.Item>
-          </Sidebar.Collapse>
-          <Sidebar.Item href="#" icon={HiInbox} className="text-white">
-            Inbox
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiUser} className="text-white">
-            Users
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiShoppingBag} className="text-white">
-            Products
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiArrowSmRight} className="text-white">
-            Sign In
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiTable} className="text-white">
-            Sign Up
-          </Sidebar.Item>
-        </Sidebar.ItemGroup>
-      </Sidebar.Items>
-    </Sidebar>
-
-  );
+function Sidebar() {
+    return (
+        <aside id="default-sidebar" className=" top-0 left-0 z-40 w-64 h-screen overflow-y-auto bg-gray-50 dark:bg-gray-800">
+            <div className="h-full px-3 py-4">
+                <ul className="space-y-2 font-medium">
+                    <li>
+                        <a href="dashboard" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <HiChartPie className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                            <span className="ms-3">Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/employee" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <HiShoppingBag className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                            <span className="flex-1 ms-3 whitespace-nowrap">
+                               Employees
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <HiInbox className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                            <span className="flex-1 ms-3 whitespace-nowrap">Inbox</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <HiUser className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                            <span className="flex-1 ms-3 whitespace-nowrap">Users</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <HiShoppingBag className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                            <span className="flex-1 ms-3 whitespace-nowrap">Products</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/hr/signin"className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <HiArrowSmRight className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                            <span className="flex-1 ms-3 whitespace-nowrap">
+                              Sign In
+                               </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/hr/signup"  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <HiTable className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                            <span className="flex-1 ms-3 whitespace-nowrap">
+                            Sign Up
+                               </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </aside>
+    );
 }
 
+export default Sidebar;
