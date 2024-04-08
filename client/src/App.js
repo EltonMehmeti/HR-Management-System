5672f710be78d2bb9ab456e3578f878e3aed385c
Syartaa <syartapajaziti@gmail.com>
2024-04-08 13:49:02 +0200

    Employee modal crud  in the client side

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,28 @@ import Dashboard from './Layouts/Dashboard/pages/Dashboard';
 import './index.css';
 import Signin from './Layouts/Dashboard/pages/Signin';
 import Signup from './Layouts/Dashboard/pages/Signup';
+import EmployeeList from './Layouts/Dashboard/pages/EmployeeList';
+
+import DashboardLayout from './Layouts/Dashboard/DashboardLayout';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+         {/* DashboardLayout will contain nested routes */}
+         <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="employee" element={<EmployeeList />} />
+          <Route path="signin" element={<Signin />} />
+        <Route path="signup" element={<Signup />} />
+        </Route>
         <Route path="/signup" element={<Signup />} />
         {/* Add more routes here if needed */}
-        <Route path="/dashboard/signin" element={<Signin />} />
-        <Route path="/dashboard/signup" element={<Signup />} />
+      
+       
+      
+       
       </Routes>
     </Router>
   );
