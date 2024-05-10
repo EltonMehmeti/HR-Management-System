d9ed7e4ae0239c28ecb333698fbf97429efbec2d
Blendi Miftari <119363582+blendimiftari@users.noreply.github.com>
2024-05-10 17:48:52 +0200

    updated image fields for interviewee, hrpersonnel and jobapplicant models

--- server/models/interview.js
+++ server/models/interview.js
@@ -1,22 +1,22 @@
 // interview.js
 
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
-const HrPersonnel = require('./hrPersonnel'); 
-const JobApplicant = require('./interviewee'); 
+const Sequelize = require("sequelize")
+const sequelize = require("../util/database")
+const HrPersonnel = require("./hrPersonnel")
+const JobApplicant = require("./interviewee")
 
-const Interview = sequelize.define('interview', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    datetime: Sequelize.DATE,
-    location: Sequelize.STRING,
-});
+const Interview = sequelize.define("interview", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  datetime: Sequelize.DATE,
+  location: Sequelize.STRING,
+})
 
-Interview.belongsTo(HrPersonnel, { foreignKey: 'recruiterId' });
-Interview.belongsTo(JobApplicant, { foreignKey: 'applicantId' });
+Interview.belongsTo(HrPersonnel, { foreignKey: "recruiterId" })
+Interview.belongsTo(JobApplicant, { foreignKey: "applicantId" })
 
-module.exports = Interview;
+module.exports = Interview
